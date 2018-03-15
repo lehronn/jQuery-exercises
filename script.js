e1acecaed597a5da6850151a358669fbeadfd5c5
Mateusz Stomski <lehronn@gmail.com>
2018-03-15 20:07:45 +0100

    Tabs works, but animate is to fix

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 console.log("jQuery exercises.");
+$(function(){
+    $(".tabs-list-element a")
+        .on('click', function () {
+            var id = this.id;
+            switch (id) {
+                case 'tab-0':
+                    $(".tabs-content")
+                        .hide("slow")
+                        .removeClass("tabs-content-active");
+
+                    $("#tabs-content-0")
+                        .delay(300)
+                        .show("slow")
+                        .addClass("tabs-content tabs-content-active");
+                    break;
+                case 'tab-1':
+                    $(".tabs-content")
+                        .hide("slow")
+                        .removeClass("tabs-content-active");
+
+                    $("#tabs-content-1")
+                        .delay(300)
+                        .show( "slow" )
+                        .addClass("tabs-content tabs-content-active");
+                    break;
+                case 'tab-2':
+                    $(".tabs-content")
+                        .hide("slow")
+                        .removeClass("tabs-content-active");
+
+                    $("#tabs-content-2")
+                        .delay(300)
+                        .show("slow")
+                        .addClass("tabs-content tabs-content-active");
+                    break;
+                case 'tab-3':
+                    $(".tabs-content")
+                        .hide("slow")
+                        .removeClass("tabs-content-active");
+
+                    $("#tabs-content-3")
+                        .delay(300)
+                        .show( "slow" )
+                        .addClass("tabs-content tabs-content-active");
+                    break;
+            }
+        });
+});
